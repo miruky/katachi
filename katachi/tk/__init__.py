a65f4a2bea3ea5157aad2c07f67b964c4ada615c
miruky <h.miyoki@gmail.com>
2025-09-30 10:59:22 +0900

    テーマ適用APIをtkバックエンドから公開

--- katachi/tk/__init__.py
+++ katachi/tk/__init__.py
@@ -2,5 +2,6 @@
 
 from .dialog import edit
 from .form import Form
+from .theme import Palette, apply_theme, current_palette, detect_mode
 
-__all__ = ["Form", "edit"]
+__all__ = ["Form", "Palette", "apply_theme", "current_palette", "detect_mode", "edit"]
