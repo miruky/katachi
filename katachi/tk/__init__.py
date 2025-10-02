d3eac3ba769117ae0b15c96745709c8b9c9901a4
miruky <h.miyoki@gmail.com>
2025-10-02 12:22:22 +0900

    ScrollableFrameを公開する

--- katachi/tk/__init__.py
+++ katachi/tk/__init__.py
@@ -2,6 +2,15 @@
 
 from .dialog import edit
 from .form import Form
+from .scroll import ScrollableFrame
 from .theme import Palette, apply_theme, current_palette, detect_mode
 
-__all__ = ["Form", "Palette", "apply_theme", "current_palette", "detect_mode", "edit"]
+__all__ = [
+    "Form",
+    "Palette",
+    "ScrollableFrame",
+    "apply_theme",
+    "current_palette",
+    "detect_mode",
+    "edit",
+]
